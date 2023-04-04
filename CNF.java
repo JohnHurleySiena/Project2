f6886a66b92e97e4a5c0b6c22d8583c005834a4f
JohnHurleySiena <jm12hurl@siena.edu>
2023-04-03 21:43:03 -0400

    Attempts

--- CNF.java
+++ CNF.java
@@ -1,1 +1,103 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Stack;
 
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class PropositionalTree {
+    Node root;
+    int size;
+
+    public PropositionalTree() {
+        root = null;
+        size = 0;
+    }
+
+    public void insert(int data, Stack<Node> stack) {
+        Node current = root;
+        Node parent = null;
+        boolean isLeftChild = false;
+
+        while (current != null) {
+            parent = current;
+            if (data < current.data) {
+                current = current.left;
+                isLeftChild = true;
+            } else if (data > current.data) {
+                current = current.right;
+                isLeftChild = false;
+            } else {
+                // Data already exists in the tree
+                return;
+            }
+        }
+
+        // Create a new node with the given data
+        Node newNode = new Node(data);
+
+        // Update the parent pointer
+        if (parent == null) {
+            root = newNode;
+        } else if (isLeftChild) {
+            parent.left = newNode;
+        } else {
+            parent.right = newNode;
+        }
+
+        // Push the new node onto the stack
+        stack.push(newNode);
+
+        // Increment the size of the tree
+        size++;
+    }
+
+    public void printInorder(Node Node) {
+        if (Node == null)
+            return;
+
+        printInorder(Node.left);
+        System.out.print(Node.data + " ");
+        printInorder(Node.right);
+    }
+
+    public void readPropositionsFromFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        Stack<Node> stack = new Stack<Node>();
+
+        while (scanner.hasNextLine()) {
+            int data = Integer.parseInt(scanner.nextLine());
+            insert(data, stack);
+        }
+
+        scanner.close();
+
+        // Print the stack
+        System.out.println("Variables in the stack: " + stack);
+    }
+
+    public static void main(String[] args) {
+        PropositionalTree tree = new PropositionalTree();
+        String fileName = "C:\\Users\\johnh\\Desktop\\list.txt";
+
+        try {
+            tree.readPropositionsFromFile(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            return;
+        }
+
+        System.out.println("Inorder traversal of propositional tree is: ");
+        tree.printInorder(tree.root);
+        System.out.println("\nTotal number of nodes in propositional tree is: " + tree.size);
+    }
+}
