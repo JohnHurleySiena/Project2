cefda72f41bf09e9254cbf9707edce3bbd98ba1c
jhurley <jm12hurl@siena.edu>
2023-04-04 13:15:24 -0400

    Started PL and combing throuhg ArrayList input

--- PL.java
+++ PL.java
@@ -7,3 +7,53 @@
 // P, R, S, ~T, U
 //
 //
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PL {
+
+
+    public static void main(String[] args) {
+
+
+        String fileName = "Z:\\nList.txt";
+
+
+        ArrayList<Character> charList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            int c;
+            while ((c = br.read()) != -1) {
+                char ch = (char) c;
+
+
+                if(ch != ',' & ch != ' '){
+                charList.add(ch);
+                }
+
+            }
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+
+        for(int i = 0; i < charList.size(); i++){
+            char temp = charList.get(i);
+
+            for(int j = 0; j < charList.size(); j++){
+
+                if(temp == charList.get(j) & charList.get(j - 1) == '~'){
+                    System.out.println("Removed " + temp + " and " + charList.get(j) + " and " + charList.get(j - 1));
+                    charList.remove(temp);
+                    charList.remove(charList.get(j));
+                    charList.remove(charList.get(j-1));
+                }
+            }
+        }
+
+        System.out.println(charList);
+    }
+}
